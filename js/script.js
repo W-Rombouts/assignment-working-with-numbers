3246d807a3607c6fb7af470558b73712ba0e2e36
Wouter Rombouts <wouter_rombouts@hotmail.com>
2020-03-09 09:07:41 +0100

    innit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+
+
+let arr = []
+for (let i = 0; i < 500; i++) {
+    if (arr<250) {
+        arr.push((Math.random()*290)+10)
+    }
+    else{
+        arr.push((Math.random()*200)+500)
+    }
+    
+}
+
+
+
+
