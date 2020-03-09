7f580db2a5834dd5e3de3abe7c5e93e19aafd2a5
Wouter Rombouts <wouter_rombouts@hotmail.com>
2020-03-09 09:34:12 +0100

    Finished

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-
-
-
 let arr = []
 for (let i = 0; i < 500; i++) {
     if (arr<250) {
@@ -12,6 +9,23 @@ for (let i = 0; i < 500; i++) {
     
 }
 
+let highIndex = 0;
+let highValue = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i]>highValue) {
+        highValue = arr[i]
+        highIndex = i
+    }
+    
+}
+
+let aantalGetallen = arr.length
+let output = `Uit het array van ${aantalGetallen} getallen is het getal ${highValue} het grootst. Dit getal zit in index ${highIndex} in het array.`
+
+let mijnpara = document.createElement('p');
+mijnpara.innerText = output
+document.body.appendChild(mijnpara)
 
 
 
